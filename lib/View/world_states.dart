88bb231150f4c8fd9436571fbd17b48de4636fa0
maheshprasad <mahesh223prasad@gmail.com>
2022-04-07 18:32:16 +0530

    initial

---
+++ lib/View/world_states.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorlsStatesScreen extends StatefulWidget {
+   WorlsStatesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WorlsStatesScreen> createState() => _WorlsStatesScreenState();
+}
+
+class _WorlsStatesScreenState extends State<WorlsStatesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
